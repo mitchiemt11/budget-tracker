ed7ef57cadd23b62820dc85efb7726d3747020d2
Mitchell <mitchiemt11@gmail.com>
2021-09-06 19:10:26 +0200

    initial commit

---
+++ src/components/AddExpenseForm.js
@@ -0,0 +1,61 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+import { v4 as uuidv4 } from 'uuid';
+
+const AddExpenseForm = () => {
+    const { dispatch } = useContext(AppContext);
+
+    const [name, setName] = useState('');
+    const [cost, setCost] = useState('');
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        const expense = {
+            id: uuidv4(),
+            name: name,
+            cost: parseInt(cost),
+        }
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense,
+        })
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <div className="row">
+                <div className="col-sm">
+                    <label for="name">Name</label>
+                    <input 
+                       type="text" 
+                       className="form-control"
+                       id="name"
+                       value={name}
+                       onChange={(event) => setName(event.target.value)}
+                       required="required"
+                    />
+                </div>
+                <div className="col-sm">
+                    <label for="cost">Cost</label>
+                    <input 
+                       type="text" 
+                       className="form-control"
+                       id="cost"
+                       value={cost}
+                       onChange={(event) => setCost(event.target.value)}
+                       required="required"
+                    />
+                </div>
+                <div className="col-sm">
+                    <button type="submit" className="btn btn-primary">
+                        Save
+                    </button>
+                </div>
+            </div>
+        </form>
+    )
+}
+
+export default AddExpenseForm
