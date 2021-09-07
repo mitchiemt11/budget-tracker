4b935116b66f76ff9ea3a9ffc1df18a549251b07
Mitchell <mitchiemt11@gmail.com>
2021-09-07 15:09:30 +0200

    Add comments

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,5 +1,6 @@
 import { createContext, useReducer } from "react";
 
+// 5. The reduceer - this is used to update the state, based on the action
 const AppReducer = (state, action) => {
     switch(action.type) {
         case 'ADD_EXPENSE':
@@ -19,6 +20,7 @@ const AppReducer = (state, action) => {
     }
 }
 
+// 1. Sets the initial state when the app loads
 const initialState = {
     budget: 2000,
     expenses: [
@@ -27,11 +29,16 @@ const initialState = {
     ]
 }
 
+// 2. Creates the context this is the thing our components import and use to get the state
 export const AppContext = createContext();
 
+// 3. Provider component - wraps the components we want to give access to the state
+// Accepts the children, which are the nested(wrapped) components
 export const AppProvider = (props) => {
+    // 4. Sets up the app state. takes a reducer, and an initial state
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
+    // 5. Returns our context. Pass in the values we want to expose
     return(
         <AppContext.Provider
            value={{
